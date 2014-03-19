8f24ef207ea5d7df97879e52cc494c066870869a
xilence2210 <xilence2210@gmail.com>
2014-03-19 17:34:58 +0800

    Chinese T9 support
    Add Chinese T9 support by SmartDialMap interface.
     * Support both Chinese and Latin Contacts Name.
     * Convert Chinese by lib pinyin4android.jar from
       http://code.google.com/p/pinyin4android/
    
    Change-Id: I27aa472587ff3361a24361a3045fe76fe7d5b1ca
    
    Conflicts:
    	Android.mk

--- Android.mk
+++ Android.mk
@@ -28,6 +28,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v13 \
     android-support-v4 \
     android-ex-variablespeed \
+    pinyin \
 
 LOCAL_REQUIRED_MODULES := libvariablespeed
 
@@ -39,5 +40,9 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags $(incallui_dir)/proguard.flags
 
 include $(BUILD_PACKAGE)
 
-# Use the following include to make our test apk.
-include $(call all-makefiles-under,$(LOCAL_PATH))
+# include pinyin lib
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := pinyin:libs/pinyin4android.jar
+
+include $(BUILD_MULTI_PREBUILT)
